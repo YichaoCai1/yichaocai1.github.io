be15e078c240c8d8440431a1b1b5ee786db33cd6
YichaoCai1 <yichao.cai@adelaide.edu.au>
2025-02-23 04:55:06 +0000

    Deploying to gh-pages from @ YichaoCai1/yichaocai1.github.io@62711b22f3aef7b1e70a70a627d8f01d6c8dd555 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "selected",
+        },{id: "nav-repos",
+          title: "repos",
+          description: "Code repositories",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -36,6 +36,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
           },
         },{id: "post-the-generalization-specialization-dilemma",
       
@@ -106,7 +113,52 @@ ninja.data = [{
     },{id: "news-our-work-clap-isolating-content-from-style-through-contrastive-learning-with-augmented-prompts-is-accepted-to-appear-at-eccv-2024",
           title: 'Our work, CLAP: Isolating Content from Style through Contrastive Learning with Augmented Prompts,...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
+            },},{
         id: 'social-discord',
         title: 'Discord',
         section: 'Socials',
